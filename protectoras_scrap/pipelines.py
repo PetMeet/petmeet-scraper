3b234320d3acbbb71f74c867bba2ae66ce341f2f
brunoLPZ <bruno.lopez.trigo@gmail.com>
2019-10-13 20:50:22 +0200

    Creado nuevo pipeline para enviar datos a cola de rabbitmq

--- protectoras_scrap/pipelines.py
+++ protectoras_scrap/pipelines.py
@@ -1,11 +1,47 @@
 # -*- coding: utf-8 -*-
+import pika
+import json
+from scrapy.utils.serialize import ScrapyJSONEncoder
 
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
 class ProtectorasScrapPipeline(object):
+
+    def __init__(self, host, port, user, password, routing_key):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.routing_key = routing_key
+
+        credentials = pika.PlainCredentials(self.user, self.password)
+
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host, port=self.port, credentials=credentials))
+        self.channel = self.connection.channel()
+
+        self.channel.queue_declare(queue=self.routing_key)
+
+        self.encoder = ScrapyJSONEncoder(ensure_ascii=False)
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            host=crawler.settings.get('RABBITMQ_HOST'),
+            port=crawler.settings.get('RABBITMQ_PORT'),
+            user=crawler.settings.get('RABBITMQ_USER'),
+            password=crawler.settings.get('RABBITMQ_PASS'),
+            routing_key=crawler.settings.get('RABBITMQ_ROUTING_KEY')
+        )
+
+    def close_spider(self, spider):
+        self.channel.close()
+        self.connection.close()
+
     def process_item(self, item, spider):
-        return item
+        data = self.encoder.encode(item)
+        print(data)
+        self.channel.basic_publish(exchange='',
+                      routing_key=self.routing_key,
+                      body=data)
--- protectoras_scrap/settings.py
+++ protectoras_scrap/settings.py
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'protectoras_scrap.pipelines.ProtectorasScrapPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'protectoras_scrap.pipelines.ProtectorasScrapPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -88,3 +88,11 @@ ROBOTSTXT_OBEY = False
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+FEED_EXPORT_ENCODING = 'utf-8'
+
+RABBITMQ_HOST = 'localhost'
+RABBITMQ_PORT = 5672
+RABBITMQ_USER = 'guest'
+RABBITMQ_PASS = 'guest'
+RABBITMQ_ROUTING_KEY = 'protectoras'
